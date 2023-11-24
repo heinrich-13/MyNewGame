28518ab64f12aadbeb8890ce850c0781a03bd9f1
Heinrich Stoltz <heinrichstoltz@icloud.com>
2023-11-24 09:52:36 +0200

    First commit

---
+++ js/classes/Player.js
@@ -0,0 +1,115 @@
+class Player extends Sprite {
+	constructor({position, collisionBlocks, imageSrc, frameRate, scale = 0.5}) {
+    super({ imageSrc, frameRate, scale })
+		this.position = position
+		this.velocity = {
+			x: 0,
+			y: 1
+		}
+    this.collisionBlocks = collisionBlocks
+    this.hitbox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      width: 10,
+      height: 10,
+    }
+
+	}
+
+	update() {
+    this.updateFrames()
+    this.updateHitbox()
+
+    c.fillStyle = "green"
+    c.fillRect(
+      this.hitbox.position.x,
+      this.hitbox.position.y,
+      this.hitbox.width,
+      this.hitbox.height
+    )
+
+		this.draw()
+		this.position.x += this.velocity.x
+    this.updateHitbox()
+    this.checkForHorizontalCollisions()
+    this.applyGravity()
+    this.updateHitbox()
+    this.checkForVerticalCollisions()
+	}
+
+  updateHitbox() {
+    this.hitbox = {
+      position: {
+        x: this.position.x + 35,
+        y: this.position.y + 26,
+      },
+      width: 14,
+      height: 27,
+    }
+  }
+
+  checkForHorizontalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+      if (
+        collision({
+          object1: this.hitbox,
+          object2: collisionBlock,
+        })
+      ) {
+        if (this.velocity.x > 0) {
+
+          const offset = this.hitbox.position.x - this.position.x + this.hitbox.width
+
+          this.velocity.x = 0
+          this.position.x = collisionBlock.position.x - offset - 0.01
+          break
+        }
+        if (this.velocity.x < 0) {
+
+          const offset = this.hitbox.position.x - this.position.x
+
+          this.velocity.x = 0
+          this.position.x = collisionBlock.position.x + collisionBlock.width - offset + 0.01
+          break
+        }
+      }
+    }
+  }
+
+  applyGravity() {
+    this.position.y += this.velocity.y;
+    this.velocity.y += gravity;
+  }
+
+  checkForVerticalCollisions() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+      if (
+        collision({
+          object1: this.hitbox,
+          object2: collisionBlock,
+        })
+        ) {
+          if (this.velocity.y > 0) {
+            this.velocity.y = 0
+
+            const offset = this.hitbox.position.y - this.position.y + this.hitbox.height
+
+            this.position.y = collisionBlock.position.y - offset - 0.01
+            break
+          }
+        if (this.velocity.y < 0) {
+          this.velocity.y = 0
+
+          const offset = this.hitbox.position.y - this.position.y
+
+          this.position.y = collisionBlock.position.y - collisionBlock.height -offset + 0.01
+          break
+        }
+      }
+    }
+  }
+}
---
+++ main.js
@@ -0,0 +1,135 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+const scaledCanvas = {
+	width: canvas.width / 4,
+	height: canvas.height / 4,
+}
+
+const floorCollisions2D = []
+for (let i = 0; i < floorCollisions.length; i += 36) {
+	floorCollisions2D.push(floorCollisions.slice(i, i + 36))
+}
+
+const collisionBlocks = []
+floorCollisions2D.forEach((row, y) => {
+	row.forEach((symbol, x) => {
+		if (symbol === 202) {
+			collisionBlocks.push(
+				new CollisionBlock({
+					position: {
+						x: x * 16,
+						y: y * 16,
+					},
+				})
+			)
+		}
+	})
+})
+
+const platformCollisions2D = []
+for (let i = 0; i < platformCollisions.length; i+= 36) {
+	platformCollisions2D.push(platformCollisions.slice(i, i + 36))
+}
+
+const platformCollisionBlocks = []
+platformCollisions2D.forEach((row, y) => {
+	row.forEach((symbol, x) => {
+		if (symbol === 202) {
+			platformCollisionBlocks.push(
+				new CollisionBlock({
+					position: {
+						x: x * 16,
+						y: y * 16,
+					},
+				})
+			)
+		}
+	})
+})
+
+
+const gravity = 0.5;
+
+const player = new Player({
+	position: {
+		x: 100,
+		y: 330
+	},
+	collisionBlocks,
+	imageSrc: "./img/warrior/Idle.png",
+	frameRate: 8
+})
+
+const keys = {
+	ArrowRight: {
+		pressed: false,
+	},
+	ArrowLeft: {
+		pressed: false
+	},
+}
+
+const background = new Sprite({
+	position: {
+		x: 0,
+		y: 0,
+	},
+	imageSrc: "./img/background.png"})
+
+function animate() {
+	requestAnimationFrame(animate);
+	c.fillStyle = "white";
+	c.fillRect(0, 0, canvas.width, canvas.height);
+
+	c.save();
+	c.scale(4, 4);
+	c.translate(0, -background.image.height + scaledCanvas.height)
+	background.update();
+
+	collisionBlocks.forEach((collisionBlock) => {
+		collisionBlock.update();
+	})
+
+	platformCollisionBlocks.forEach((collisionBlock) => {
+		collisionBlock.update();
+	})
+
+	player.update();
+
+	player.velocity.x = 0;
+	if (keys.ArrowRight.pressed) player.velocity.x = 3
+	else if (keys.ArrowLeft.pressed) player.velocity.x = -3
+
+	c.restore();
+}
+
+animate();
+
+window.addEventListener("keydown", (event) => {
+	switch (event.key) {
+		case "ArrowRight":
+			keys.ArrowRight.pressed = true;
+			break;
+		case "ArrowLeft":
+			keys.ArrowLeft.pressed = true;
+			break;
+		case "ArrowUp":
+			player.velocity.y = -8;
+			break;
+	}
+})
+
+window.addEventListener("keyup", (event) => {
+	switch (event.key) {
+		case "ArrowRight":
+			keys.ArrowRight.pressed = false;
+			break;
+		case "ArrowLeft":
+			keys.ArrowLeft.pressed = false;
+			break;
+	}
+})
